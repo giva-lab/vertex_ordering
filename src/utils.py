4db114c68f4bd376381e21ec6a409f6590252132
victorhb <victorhb@icmc.usp.br>
2025-02-27 15:31:44 -0300

    Removing breakpoints from utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,25 +3,17 @@ from scipy.sparse import csr_matrix
 
 def get_topological_measure_optimized(A, order):
     # Get nonzero indices and values
-    breakpoint()
     rows, cols = A.nonzero()
-    breakpoint()
     order = np.array(order)
-    breakpoint()
     # Get order differences for all nonzero connections
     diffs = np.abs(order[cols] - order[rows])
-    breakpoint()
     # Sum connections per row
     connections = np.diff(A.indptr)
-    breakpoint()
     # Reshape and get maximum differences per row
     max_diffs = np.zeros(A.shape[0])
-    breakpoint()
     np.maximum.at(max_diffs, rows, diffs)
-    breakpoint()
     # Compute the result as per formula
     res = max_diffs / connections
-    breakpoint()
     return res
 
 def get_topological_measure(A, order):
